c381164112cefc2d225c7815a29fe37f9b1878c1
Jerome Bakker <jeabakker@coldtrick.com>
2014-07-17 09:42:22 +0200

    dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file was created by Translation Editor v2.0
+ * On 2014-07-17 09:39
+ */
+
+$language = array (
+  'plugin_manager:plugins:label:info' => 'Info',
+  'plugin_manager:plugins:label:files' => 'Bestanden',
+  'plugin_manager:plugins:label:screenshots' => 'Screenshots',
+  'plugin_manager:plugins:label:resources' => 'Links',
+  'admin:plugins:label:name' => 'Naam',
+  'admin:plugins:label:id' => 'interne id',
+  'admin:plugins:label:license' => 'Licentie',
+);
+add_translation("nl", $language);
